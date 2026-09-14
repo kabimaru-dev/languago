9df690ae3024fc85c780c7a9b59a9023f74e96f7
kabimaru-dev <viacheslavchubenko@animesakura.net>
2026-09-14 21:58:10 +0300

    Add: CRA pattern and response. So, Now, Node.js + React.js are together (In Theory(Not Really))

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ const send_response = function (request, response) {
   console.log(request.method, request.url);
   
   if (request.url === "/") {
+    const page = fs.readFileSync("./views/App.js", "utf8");
+
     response.writeHead(200, {
       "Content-Type": "text/html; charset=utf-8"  
     });
 
-    response.end("<h1>/</h1>");
+    response.end(page);
   }
   else if (request.url === "/notahome") {
+    const page = fs.readFileSync("./views/App.js", "utf8");
+
     response.writeHead(200, {
       "Content-Type": "text/html; charset=utf-8"  
     });
 
-    response.end("<h1>/notahome</h1>");
+    response.end(page);
   }
   else {
     response.end("<h1>Wrong page!</h1>");
